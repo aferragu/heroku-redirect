eeffdb85cf634319b2d609f15eae643d9a212ec9
Andres Ferragut <andres.ferragut@gmail.com>
2021-03-01 23:01:29 -0300

    hardcoded address

--- web.js
+++ web.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-var newBaseURL = process.env.NEW_BASE_URL || 'http://example.com';
-var redirectStatus = parseInt(process.env.REDIRECT_STATUS || 302);
+var newBaseURL = 'https://covid-monitor.agesic.gub.uy';
+var redirectStatus =  302;
 var port = process.env.PORT || 5000;
 
 app.get('*', function(request, response) {
